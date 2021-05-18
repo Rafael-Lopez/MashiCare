4a6497029fe66188ec6cbb3dde584bc94c71bb4f
Rafael Lopez <rafael.lopez@bbd.ca>
2021-05-17 21:07:23 -0400

    MAS-7: As a Developer, I would like to add CORS configuration to allow the Angular client to talk to the Spring Boot project

--- src/main/java/com/lopez/rafael/mashicare/config/SecurityConfiguration.java
+++ src/main/java/com/lopez/rafael/mashicare/config/SecurityConfiguration.java
@@ -9,14 +9,32 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                    .configurationSource(new CorsConfigurationSource() {
+                        @Override
+                        public CorsConfiguration getCorsConfiguration(HttpServletRequest httpServletRequest) {
+                            CorsConfiguration corsConfig = new CorsConfiguration();
+                            corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                            corsConfig.setAllowedMethods(Collections.singletonList("*"));
+                            corsConfig.setAllowCredentials(true);
+                            corsConfig.setAllowedHeaders(Collections.singletonList("*"));
+
+                            return corsConfig;
+                        }
+                    })
+                .and()
                 // Disable Spring Security's default CSRF Protection
                 .csrf().disable()
                 .authorizeRequests()
@@ -24,6 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .mvcMatchers(HttpMethod.GET, "/medicine/*").permitAll()
                 .mvcMatchers("/medicine").hasRole("ADMIN")
                 .mvcMatchers("/medicine/*").hasRole("ADMIN")
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .and()
---
+++ src/main/java/com/lopez/rafael/mashicare/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.lopez.rafael.mashicare.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserController {
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+}
