30863396f805dc1cd864800835aeb509f79f3c60
Rafael Lopez <rafael.lopez@bbd.ca>
2021-05-26 20:14:26 -0400

    MAS-14: As a Developer, I would like to remove the debug messages from Spring Security

--- src/main/java/com/lopez/rafael/mashicare/MashicareApplication.java
+++ src/main/java/com/lopez/rafael/mashicare/MashicareApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class MashicareApplication {
 
     public static void main(String[] args) {
